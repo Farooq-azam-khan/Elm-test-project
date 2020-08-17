961bfc6854f55dc5182ec7f174e4ded6d8e0debf
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-08-16 22:09:17 -0400

    made request to external site

--- src/Main.elm
+++ src/Main.elm
@@ -1,92 +1,135 @@
 module Main exposing (main)
 
+-- import Html.Events exposing (onClick, onInput)
+-- import Html.Attributes exposing (..)
+
 import Browser
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing (onClick, onInput)
-import List
+import Html exposing (Html, pre, text)
+import Http
 
 
 
+-- import List
 -- Main
 
 
 main =
-    Browser.sandbox { init = init, update = update, view = view }
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
 
 
 
 -- Model
+-- type alias Model =
+--     { counter : Int, content : String, authors : List String, newAuthor : String }
 
 
-type alias Model =
-    { counter : Int, content : String, authors : List String, newAuthor : String }
+type Model
+    = Failure
+    | Loading
+    | Success String
 
 
-init : Model
-init =
-    { counter = 0, content = "", authors = [], newAuthor = "" }
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Loading
+    , Http.get
+        { url = "https://elm-lang.org/assets/public-opinion.txt"
+        , expect = Http.expectString GotText
+        }
+    )
 
 
 
+-- init =
+--     { counter = 0, content = "", authors = [], newAuthor = "" }
 -- update
 
 
 type Msg
-    = Increment
-    | Decrement
-    | Reset
-    | Increment10
-    | AddAuthor
-    | UpdateAuthorName String
+    = GotText (Result Http.Error String)
 
 
-update : Msg -> Model -> Model
+
+-- type Msg
+--     = Increment
+--     | Decrement
+--     | Reset
+--     | Increment10
+--     | AddAuthor
+--     | UpdateAuthorName String
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Increment ->
-            { model | counter = model.counter + 1, content = "Added 1" }
+        GotText result ->
+            case result of
+                Ok fullText ->
+                    ( Success fullText, Cmd.none )
 
-        Increment10 ->
-            { model | counter = model.counter + 10, content = "Added 10" }
-
-        Decrement ->
-            if model.counter - 1 >= 0 then
-                { model | counter = model.counter - 1, content = "deleted 1" }
-
-            else
-                { model | counter = model.counter, content = "Cannot go below zero" }
-
-        UpdateAuthorName name ->
-            { model | newAuthor = name }
-
-        AddAuthor ->
-            { model | authors = model.newAuthor :: model.authors, newAuthor = "" }
-
-        Reset ->
-            { model | counter = 0, content = "Reset" }
+                Err _ ->
+                    ( Failure, Cmd.none )
 
 
 
+-- update msg model =
+--     case msg of
+--         Increment ->
+--             { model | counter = model.counter + 1, content = "Added 1" }
+--         Increment10 ->
+--             { model | counter = model.counter + 10, content = "Added 10" }
+--         Decrement ->
+--             if model.counter - 1 >= 0 then
+--                 { model | counter = model.counter - 1, content = "deleted 1" }
+--             else
+--                 { model | counter = model.counter, content = "Cannot go below zero" }
+--         UpdateAuthorName name ->
+--             { model | newAuthor = name }
+--         AddAuthor ->
+--             { model | authors = model.newAuthor :: model.authors, newAuthor = "" }
+--         Reset ->
+--             { model | counter = 0, content = "Reset" }
 -- view
-
-
-showAuthor name acc =
-    li [] [ text name ] :: acc
+-- showAuthor name acc =
+--     li [] [ text name ] :: acc
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (String.fromInt model.counter) ]
-        , div [] [ text model.content ]
-        , button [ onClick Increment ] [ text "+" ]
-        , button [ onClick Increment10 ] [ text "+10" ]
-        , button [ onClick Reset ] [ text "Reset" ]
-        , h1 [] [ text "Authors" ]
-        , input [ placeholder "Author Name", value model.newAuthor, Html.Events.onInput UpdateAuthorName ] []
-        , button [ onClick AddAuthor ] [ text "Add Author" ]
-        , ul []
-            (List.foldl showAuthor [] model.authors)
-        ]
+    case model of
+        Failure ->
+            text "Unable to load book."
+
+        Loading ->
+            text "laoding..."
+
+        Success fullText ->
+            pre [] [ text fullText ]
+
+
+
+-- view model =
+--     div []
+--         [ button [ onClick Decrement ] [ text "-" ]
+--         , div [] [ text (String.fromInt model.counter) ]
+--         , div [] [ text model.content ]
+--         , button [ onClick Increment ] [ text "+" ]
+--         , button [ onClick Increment10 ] [ text "+10" ]
+--         , button [ onClick Reset ] [ text "Reset" ]
+--         , h1 [] [ text "Authors" ]
+--         , input [ placeholder "Author Name", value model.newAuthor, Html.Events.onInput UpdateAuthorName ] []
+--         , button [ onClick AddAuthor ] [ text "Add Author" ]
+--         , ul []
+--             (List.foldl showAuthor [] model.authors)
+--         ]
+-- subscriptions
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
