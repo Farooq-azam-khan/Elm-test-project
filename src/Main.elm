53abea3a740a6964b3faeb30bda343bb9fdbce8e
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-08-18 23:56:18 -0400

    Update Main.elm
    started book reading/tracking application

--- src/Main.elm
+++ src/Main.elm
@@ -1,135 +1,125 @@
 module Main exposing (main)
 
--- import Html.Events exposing (onClick, onInput)
--- import Html.Attributes exposing (..)
-
 import Browser
-import Html exposing (Html, pre, text)
-import Http
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+import List
+import Round
 
 
 
--- import List
 -- Main
 
 
 main =
-    Browser.element
+    Browser.sandbox
         { init = init
         , update = update
-        , subscriptions = subscriptions
         , view = view
         }
 
 
 
 -- Model
--- type alias Model =
---     { counter : Int, content : String, authors : List String, newAuthor : String }
 
 
-type Model
-    = Failure
-    | Loading
-    | Success String
+type alias Author =
+    { name : String }
 
 
-init : () -> ( Model, Cmd Msg )
-init _ =
-    ( Loading
-    , Http.get
-        { url = "https://elm-lang.org/assets/public-opinion.txt"
-        , expect = Http.expectString GotText
-        }
-    )
+type alias Book =
+    { name : String, authors : List Author, pages : Int, chapters : Int }
+
+
+type alias Model =
+    { books : List Book, authors : List Author, show_authors : Bool }
+
+
+init : Model
+init =
+    { books =
+        [ book1
+        , book2
+        ]
+    , authors = [ Author "George R. R. Martin" ]
+    , show_authors = False
+    }
+
+
+book1 =
+    Book "A Game of Thrones" [ Author "George R. R. Martin" ] 800 50
+
+
+book2 =
+    Book "A Storm of Swords" [ Author "George R. R. Martin" ] 1128 90
 
 
 
--- init =
---     { counter = 0, content = "", authors = [], newAuthor = "" }
 -- update
 
 
 type Msg
-    = GotText (Result Http.Error String)
+    = ShowAuthors
 
 
-
--- type Msg
---     = Increment
---     | Decrement
---     | Reset
---     | Increment10
---     | AddAuthor
---     | UpdateAuthorName String
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
+update : Msg -> Model -> Model
 update msg model =
     case msg of
-        GotText result ->
-            case result of
-                Ok fullText ->
-                    ( Success fullText, Cmd.none )
-
-                Err _ ->
-                    ( Failure, Cmd.none )
-
-
-
--- update msg model =
---     case msg of
---         Increment ->
---             { model | counter = model.counter + 1, content = "Added 1" }
---         Increment10 ->
---             { model | counter = model.counter + 10, content = "Added 10" }
---         Decrement ->
---             if model.counter - 1 >= 0 then
---                 { model | counter = model.counter - 1, content = "deleted 1" }
---             else
---                 { model | counter = model.counter, content = "Cannot go below zero" }
---         UpdateAuthorName name ->
---             { model | newAuthor = name }
---         AddAuthor ->
---             { model | authors = model.newAuthor :: model.authors, newAuthor = "" }
---         Reset ->
---             { model | counter = 0, content = "Reset" }
--- view
--- showAuthor name acc =
---     li [] [ text name ] :: acc
+        ShowAuthors ->
+            { model | show_authors = not model.show_authors }
 
 
 view : Model -> Html Msg
 view model =
-    case model of
-        Failure ->
-            text "Unable to load book."
+    div [ class "bg-gray-200", class "text-gray-800" ]
+        [ renderBooks model.books
+        , if model.show_authors then
+            renderAuthors model.authors
 
-        Loading ->
-            text "laoding..."
-
-        Success fullText ->
-            pre [] [ text fullText ]
-
-
-
--- view model =
---     div []
---         [ button [ onClick Decrement ] [ text "-" ]
---         , div [] [ text (String.fromInt model.counter) ]
---         , div [] [ text model.content ]
---         , button [ onClick Increment ] [ text "+" ]
---         , button [ onClick Increment10 ] [ text "+10" ]
---         , button [ onClick Reset ] [ text "Reset" ]
---         , h1 [] [ text "Authors" ]
---         , input [ placeholder "Author Name", value model.newAuthor, Html.Events.onInput UpdateAuthorName ] []
---         , button [ onClick AddAuthor ] [ text "Add Author" ]
---         , ul []
---             (List.foldl showAuthor [] model.authors)
---         ]
--- subscriptions
+          else
+            button
+                [ onClick ShowAuthors
+                , class "bg-blue-800 hover:bg-blue-900 text-white px-4 py-2 text-sm font-semibold"
+                ]
+                [ text "Show Author" ]
+        ]
 
 
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    Sub.none
+renderBooks : List Book -> Html Msg
+renderBooks books =
+    div [ class "md:container mx-auto" ]
+        [ h1 []
+            [ text "Books" ]
+        , div
+            [ class "flex justify-center space-x-10 space-y-2 items-center" ]
+            (List.map renderBookRow books)
+        ]
+
+
+renderBookRow : Book -> Html Msg
+renderBookRow book =
+    div [ class "bg-gray-800 hover:bg-gray-900 rounded-lg shadow-xl space-x-3 text-gray-200 px-3 py-2" ]
+        [ h3 [ class "text-xl" ] [ text book.name ]
+        , ul [ class "flex items-center  w-full space-x-2" ] (List.map (\author -> li [] [ a [ href "#", class "hover:underline hover:text-orange-400" ] [ text author.name ] ]) book.authors)
+        , p [ class "flex items-center jusitfy-between space-x-2 w-full" ]
+            [ span [] [ text (String.fromInt book.pages ++ " (" ++ Round.round 3 (100 / Basics.toFloat book.pages) ++ "%)") ]
+            , span [ class "text-xs text-gray-400 font-semibold uppercase" ] [ text "pages" ]
+            ]
+        , p [ class "flex items-center jusitfy-between space-x-2 w-full" ]
+            [ span [] [ text (String.fromInt book.chapters ++ " (" ++ Round.round 3 (100 / Basics.toFloat book.chapters) ++ "%)") ]
+            , span [ class "text-xs text-gray-400 font-semibold uppercase" ] [ text "chaps" ]
+            ]
+        ]
+
+
+renderAuthors : List Author -> Html Msg
+renderAuthors authors =
+    div []
+        [ h1 [] [ text "Autors" ]
+        , div [] [ ul [] (List.map renderAuthor authors) ]
+        ]
+
+
+renderAuthor author =
+    li [] [ text author.name ]
