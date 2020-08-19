d58fa648d5392e4cfddc3ca2f0bd76ba3aa63e70
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-08-18 23:57:53 -0400

    Generating Pipeline
    Outputing elm into js file and in index.html file loading in compiled css.
    Also added tailwindcss to pipeline

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+    purge: {
+        enabled: false,
+        content: ['./src/**/*.html',
+            './src/**/*.elm',
+            './src/**/*.jsx',
+        ],
+    },
+    theme: {},
+    variants: {},
+    plugins: [],
+}
